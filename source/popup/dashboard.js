67b0305eff4a8a2670891877c20531911df0fe18
Mariano Lambir <mlambir@gmail.com>
2021-06-09 11:53:33 -0300

    add bootstrap

--- source/popup/dashboard.js
+++ source/popup/dashboard.js
@@ -1,10 +1,11 @@
-import {saveDomains, getDomains, saveCategories} from "../options/storage"
+import {saveDomains, getDomains, saveCategories, getCategories} from "../options/storage"
 
 saveDomains([{
   domain: 'github.com', 
   category: '', 
   enabled: true
 }]);
+
 saveCategories([{
   name: "distraction",
   color: "#CC0000",
@@ -17,5 +18,26 @@ document.querySelector("#options-button").addEventListener("click", (e) => {
 })
 
 document.querySelector("#block-domain").addEventListener("click", (e) => {
-    browser.runtime.openOptionsPage();
+  browser.tabs.query({active: true, windowId: browser.windows.WINDOW_ID_CURRENT})
+  .then(tabs => browser.tabs.get(tabs[0].id))
+  .then(tab => {
+    let url = new URL(tab.url);
+    document.getElementById("domain-input").value = url.hostname;
+    document.getElementById("add-domain").hidden = false;
+  });
 })
+
+
+document.onLoad = () =>{
+  getCategories().then(categories=>{
+    let categorySelect = document.getElementById("category-select");
+    console.log(categories);
+    categories.forEach(category => {
+      console.log("cat");
+      let o = document.createElement('option');
+      o.innerText = category.name;
+      o.value = category.name;
+      categorySelect.add(o)
+    });
+  });
+}
