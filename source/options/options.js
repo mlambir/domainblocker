e0b9738745917221f3cd8013c0a09a216c9ac000
Belen Cattaneo <belcattaneo@gmail.com>
2021-06-11 14:58:06 -0300

    Some styles

--- source/options/options.js
+++ source/options/options.js
@@ -338,6 +338,17 @@ getElementById("delete-category").addEventListener("click", (e) => {
 	deleteCategory(categoryData)
 });
 
+// Default options
+IconPicker.Init({
+	// Required: You have to set the path of IconPicker JSON file to "jsonUrl" option. e.g. '/content/plugins/IconPicker/dist/iconpicker-1.5.0.json'
+	jsonUrl: browser.extension.getURL("./static/iconpicker-1.5.0.json"),
+	// Optional: Change the buttons or search placeholder text according to the language.
+	searchPlaceholder: 'Search Icon',
+	showAllButton: 'Show All',
+	cancelButton: 'Cancel',
+	noResultsFound: 'No results found.', // v1.5.0 and the next versions
+	borderRadius: '20px', // v1.5.0 and the next versions
+});
 
 //saveDomains([])
 //saveCategories([])
