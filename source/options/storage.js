537686921d59b7cac61a6200ee9ce9127a5c66df
Mariano Lambir <mlambir@gmail.com>
2021-06-11 12:43:16 -0300

    several improvements

--- source/options/storage.js
+++ source/options/storage.js
@@ -38,12 +38,21 @@ export function getDomainsFull(){
       categories[category.name] = category;
     }
     return res.domains.map(d=>{
+      if(!d.enabled){
+        return null;
+      }
       let category = categories[d.category];
       if(category){
+        if(!category.enabled){
+          return null;
+        }
         d.color = category.color;
         d.icon = category.icon;
-        return d;
+      }else{
+        d.color = 'red';
+        d.icon = 'times';
       }
+      return d;
     })
   });
 }
